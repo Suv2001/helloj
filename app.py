f6cd7044266169d9f4dcfe1d5a9a5a34cf8995a8
Suvendu Dash Adhikary <159238155+Suv2001@users.noreply.github.com>
2025-08-19 12:51:41 +0530

    Update app.py
    Commit1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask! The app is running on port 3000."
+    return "Hello from Flask! The app is running on port 3000. this is for commit action checking "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
