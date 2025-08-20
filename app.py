c0ddb7141ec00a56952857747f9f22b4b0645526
Suvendu Dash Adhikary <159238155+Suv2001@users.noreply.github.com>
2025-08-20 11:17:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask! The app is running on port 3000. this is for commit action checking. If you are seeing this message that means github webhooks trigred   "
+    return "Hello from Flask! The app is running on port 3000. this is for commit action checking. If you are seeing this message that means github webhooks trigred. This message appears because it was triggered by GitHub webhooks."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
