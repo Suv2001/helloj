f75572a1a60f1e0960af465a9649960ffaed286c
Suvendu Dash Adhikary <159238155+Suv2001@users.noreply.github.com>
2025-10-04 10:41:05 +0530

    Fix typo in home route response message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello from Flask-app! The app is running on port 3000. this is for commit action checking. it was triggered by GitHub webhooks.</h1>"
+    return "<h1>Hello from Flask-app! Tvv  he app is running on port 3000. this is for commit action checking. it was triggered by GitHub webhooks.</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
